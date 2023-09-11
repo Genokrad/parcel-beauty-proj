114e617ca392f5b36e8d288fdbdf6594e5baeddc
Genokrad <zeeper90@gmail.com>
2023-09-11 13:39:42 +0300

    header

--- src/index.js
+++ src/index.js
@@ -1,154 +1,65 @@
-// import imagePlastic from './images/direction/cosmetology.png';
-// import sprite from './images/sprite.svg';
+const getEl = el => document.querySelector(el);
 
-// const element = selector => document.querySelector(selector);
-// const directions = element('.directions__desktop-wrapper');
-// console.log(directions);
+import sprite from './images/sprite.svg';
 
-// function myEventHandler(event) {
-//   // Ваш код для обработки события
-//   const actionTitleList = document.querySelectorAll(
-//     '.directions__subtitle--desktop'
-//   );
-//   if (actionTitleList !== null) {
-//     classTogler(actionTitleList, event);
-//   }
-//   // console.log(actionTitleList);
-// }
+const toggleClass = mark => {
+  console.log(mark);
+  if (mark === 'modal') {
+    toggleSvg();
+    getEl('.header-modal').classList.toggle('visually-hidden');
+  }
+  if (mark === 'servicess') {
+    toggleChevron('.header-modal__icon use');
+    getEl('.header-modal__services-list--servicess').classList.toggle(
+      'visually-hidden'
+    );
+  }
+  if (mark === 'doctors') {
+    console.log(mark);
+    toggleChevron('.header-modal__icon--doctors use');
+    getEl('.header-modal__services-list--doctors').classList.toggle(
+      'visually-hidden'
+    );
+  }
+};
 
-// // Повесьте слушатель события на объект window
-// window.addEventListener('click', myEventHandler);
+// header-modal__services-list
 
-// function throttle(callback, delay) {
-//   let timerId;
+const toggleChevron = selector => {
+  const servicess = getEl(selector);
 
-//   return function () {
-//     if (!timerId) {
-//       timerId = setTimeout(() => {
-//         callback();
-//         timerId = null;
-//       }, delay);
-//     }
-//   };
-// }
+  const currentHref = servicess.getAttribute('xlink:href');
 
-// function classTogler(actionTitleList, event) {
-//   function contain(arr, value) {
-//     let result = null;
-//     [...arr].find(item => {
-//       if (item === value) {
-//         result = true;
-//       } else {
-//         result = false;
-//       }
-//     });
-//     return result;
-//   }
+  if (currentHref === `${sprite}#chevronUp`) {
+    servicess.setAttribute('xlink:href', `${sprite}#chevronBot`);
+  } else if (currentHref === `${sprite}#chevronBot`) {
+    servicess.setAttribute('xlink:href', `${sprite}#chevronUp`);
+  } else {
+    servicess.setAttribute('xlink:href', `${sprite}#chevronBot`);
+  }
+};
 
-//   if (contain(event.target.classList, 'directions__subtitle--desktop-active')) {
-//     event.target.childNodes[2].innerHTML = '';
-//   }
+const toggleSvg = () => {
+  const hamburgerIcon = document.querySelector('.header__hamburger use');
+  const currentHref = hamburgerIcon.getAttribute('xlink:href');
 
-//   if (contain(event.target.classList, 'directions__subtitle--desktop')) {
-//     actionTitleList.forEach(element => {
-//       element.classList.remove('directions__subtitle--desktop-active');
-//       event.target.classList.add('directions__subtitle--desktop-active');
+  if (currentHref === `${sprite}#hamburger`) {
+    hamburgerIcon.setAttribute('xlink:href', `${sprite}#cross`);
+  } else if (currentHref === `${sprite}#cross`) {
+    hamburgerIcon.setAttribute('xlink:href', `${sprite}#hamburger`);
+  } else {
+    hamburgerIcon.setAttribute('xlink:href', `${sprite}#cross`);
+  }
+};
 
-//       console.dir(event.target.innerText);
-//     });
-//   }
-// }
+getEl('.header__hamburger').addEventListener('click', () =>
+  toggleClass('modal')
+);
 
-// let helloRendered = false; // Flag to track if "Hello" div has been rendered
+getEl('.button--header-modal').addEventListener('click', () =>
+  toggleClass('servicess')
+);
 
-// function renderMarkup() {
-//   if (!helloRendered) {
-//     const helloMarkup = `
-//   <div class="directions__card">
-//     <img
-//       class="directions__img"
-//       src="${imagePlastic}"
-//       alt="women"
-//     />
-//   </div>
-//   <div class="directions__card-information">
-//     <ul class="directions__action-title directions__action-title--desctop">
-
-//       <li class="directions__item-desctop directions__item-desctop--active">
-//         <p class="directions__subtitle directions__subtitle--desktop directions__subtitle--desktop-active">Пластична хірургія</p>
-//       </li>
-//       <li class="directions__item-desctop">
-//         <p class="directions__subtitle directions__subtitle--desktop">Eстетична гінекологія
-//         </p>
-//       </li>
-//       <li >
-//         <p class="directions__subtitle directions__subtitle--desktop">Косметологія
-//         </p>
-//       </li>
-//     </ul>
-
-//   </div>`;
-//     directions.insertAdjacentHTML('beforeend', helloMarkup);
-//     helloRendered = true; // Set the flag to true after rendering
-//   }
-//   renderAdditionalContent();
-// }
-
-// function watchViewportSizeAbove(callback) {
-//   const throttledCallback = throttle(callback, 300);
-
-//   window.addEventListener('resize', () => {
-//     const viewportWidth = window.innerWidth;
-
-//     // Check if viewport width is greater than or equal to 1440 pixels
-//     if (viewportWidth >= 1440) {
-//       throttledCallback(); // Call the callback function with throttling
-//     } else {
-//       // If viewport width is less than 1440 pixels, remove the "Hello" div
-//       const helloDiv = directions.querySelector('div');
-//       if (helloDiv) {
-//         helloDiv.remove();
-//         helloRendered = false; // Reset the flag when removed
-//       }
-//     }
-//   });
-// }
-
-// watchViewportSizeAbove(() => {
-//   console.log('Viewport is greater than or equal to 1440px.');
-//   renderHello(); // Render the "Hello" div when the condition is met
-// });
-
-// (function () {
-//   if (window.innerWidth >= 1440) {
-//     renderMarkup();
-//   }
-// })();
-
-// function renderAdditionalContent() {
-//   // Находим элемент с классом "directions__subtitle--desktop-active"
-//   const activeSubtitle = document.querySelector(
-//     '.directions__item-desctop--active'
-//   );
-
-//   if (activeSubtitle) {
-//     // Создаем верстку
-//     const additionalContentMarkup = `
-//       <div class="directions__card-information">
-
-//         <p class="directions__text">
-//           Пластика грудей, обличчя, тіла - все для ваших ідеальних ліній,
-//           виразного погляду та гармонійних рис
-//         </p>
-//         <button class="button button--direction">
-//           Детальніше<svg viewBox="0 0 32 32 xmlns="http://www.w3.org/2000/svg  class="button__icone">
-//             <use xlink:href="${sprite}#arrowRight"></use>
-//           </svg>
-//         </button>
-//       </div>
-//     `;
-
-//     // Вставляем созданную верстку перед элементом "activeSubtitle"
-//     activeSubtitle.insertAdjacentHTML('beforeend', additionalContentMarkup);
-//   }
-// }
+getEl('#button-doctors').addEventListener('click', () =>
+  toggleClass('doctors')
+);
--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -1,31 +1,30 @@
-const checkboxes = document.querySelectorAll('.accordion__checkbox');
-const labels = document.querySelectorAll('.accordion__label');
-const lists = document.querySelectorAll('.accordion__list');
+const checkboxContainers = document.querySelectorAll(
+  '.accordion__string-wrapper'
+);
 
 import sprite from '../images/sprite.svg';
 
-// Добавляем обработчик события для каждого чекбокса
-checkboxes.forEach((checkbox, index) => {
-  checkbox.addEventListener('change', () => {
-    // Если чекбокс отмечен, меняем иконку на минус, иначе на плюс
-    labels[index]
+checkboxContainers.forEach((container, index) => {
+  container.addEventListener('click', () => {
+    const checkbox = container.querySelector('.accordion__checkbox');
+    const label = container.querySelector('.accordion__label');
+    const list = container.querySelector('.accordion__list');
+
+    checkbox.checked = !checkbox.checked;
+
+    label
       .querySelector('use')
       .setAttribute(
         'xlink:href',
         checkbox.checked ? `${sprite}#minus` : `${sprite}#plus`
       );
 
-    // Если чекбокс отмечен, меняем цвет фона на красный, иначе возвращаем исходный цвет
-    labels[index].style.backgroundColor = checkbox.checked
+    label.style.backgroundColor = checkbox.checked
       ? '#DC3C3C'
       : 'rgba(52, 56, 69, 0.1)';
-    labels[index].style.fill = checkbox.checked ? '#ffffff' : '#1E1E2B';
+    label.style.fill = checkbox.checked ? '#ffffff' : '#1E1E2B';
 
-    // Если чекбокс отмечен, показываем список, в противном случае скрываем его
-    if (checkbox.checked) {
-      lists[index].style.display = 'grid';
-    } else {
-      lists[index].style.display = 'none';
-    }
+    // Показываем или скрываем список
+    list.style.display = checkbox.checked ? 'grid' : 'none';
   });
 });
